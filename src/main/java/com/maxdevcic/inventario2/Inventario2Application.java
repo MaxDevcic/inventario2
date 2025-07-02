a7d89311201ade5cbd1cc448d2fd72f5c5501aee
MaxDevcic <ma.devcic@duocuc.cl>
2025-07-02 12:27:48 -0400

    first commit

---
+++ src/main/java/com/maxdevcic/inventario2/Inventario2Application.java
@@ -0,0 +1,13 @@
+package com.maxdevcic.inventario2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inventario2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inventario2Application.class, args);
+	}
+
+}
